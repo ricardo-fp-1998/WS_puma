1fd80d15de0388fcabe3134dd7924a90470251a6
cardo <cardo@gmail.com>
2019-11-22 12:33:21 -0500

    subiendo archivo

---
+++ WSproject/Service1.svc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WSproject
+
+{
+    //ESCAFFOLD -> VIENE A SER PROYECTO BASE
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public string GetData(int value, int multiplicar)
+        {
+            int rpta;
+            string msj;
+            if (value > 10)
+            {
+                msj = "no se puede multiplicar numeros mayores a : " + value;
+            }
+            else
+            {
+                multiplicar = 0;
+                value = value * multiplicar;
+                rpta =  value;
+            }
+
+            return msj;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
